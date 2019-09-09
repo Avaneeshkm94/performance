bdb0b01190d336ff3bca27f7c6db154562fcb65d
avnikumar94@gmail.com <Avaneeshkm94>
2019-09-09 14:35:41 +0530

    first

---
+++ sites/default/files/php/twig/5d75fec2d2ae5___string_template__f3c202_bq01CWrHq_1V2FZ6-Am8YWi88/isf02SDqiUWa7XQ_7VFucOxvn4WM7vcWtUzY14eSuPE.php
@@ -0,0 +1,80 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* __string_template__f3c2022cad8f34e40cbf293cf2e6b6b4a492b9ad5d9c363f7778df3c85cf48ab */
+class __TwigTemplate_cd840316b113982d6e77ce413a0e09e3d29a87343f148fb334e9d8a464aaca9e extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = [];
+        $filters = [];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                [],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 1
+        echo "/node/1";
+    }
+
+    public function getTemplateName()
+    {
+        return "__string_template__f3c2022cad8f34e40cbf293cf2e6b6b4a492b9ad5d9c363f7778df3c85cf48ab";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  55 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "__string_template__f3c2022cad8f34e40cbf293cf2e6b6b4a492b9ad5d9c363f7778df3c85cf48ab", "");
+    }
+}
